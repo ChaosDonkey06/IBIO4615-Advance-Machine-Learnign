225384e63d0227f0e638e6c65ec325f7b0fd18eb
ChaosDonkey06 <jecv@Humanity-Plus.local>
2019-09-09 14:05:33 -0500

    adding corrections and code

--- RL_01/01_DQN/DQN.py
+++ RL_01/01_DQN/DQN.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,30 +7,60 @@ from torch import optim
 import matplotlib.pyplot as plt
 import gym
 
+# parser for all parameters and hyperparameters
+parser = argparse.ArgumentParser()
 
-#hyper parameters
-EPSILON = 0.9
-GAMMA = 0.9
-LR = 0.01
-MEMORY_CAPACITY = 2000
-Q_NETWORK_ITERATION = 100
-BATCH_SIZE = 32
+parser.add_argument('--mode', default='train', type=str)
+parser.add_argument("--env_name", default="MountainCar-v0")
 
+parser.add_argument('--q_network_iteration', default=100, type=int)
+parser.add_argument('--test_iteration', default=10, type=int)
+
+parser.add_argument('--learning_rate', default=10e-3, type=float) # same for critic and actor
+parser.add_argument('--gamma', default=0.9, type=int) # discount factor (is high cause dynamics are super fast)
+parser.add_argument('--capacity', default=50000, type=int) # replay buffer size (should change based on your resources)
+parser.add_argument('--max_length_of_trajectory', default=5000, type=int) # length of game
+
+parser.add_argument('--batch_size', default=32, type=int) # mini batch size (same for both critic and actor)
+parser.add_argument('--epsilon', default=0.9, type=int)
+# optional parameters
+
+parser.add_argument('--render', default=False, type=bool) # show UI (best your for test)
+parser.add_argument('--show_performance', default=True, type=bool) # show performance when train/test ends
+parser.add_argument('--log_interval', default=50, type=int) #
+parser.add_argument('--load', default=False, type=bool) # load pre-trained model
+parser.add_argument('--render_interval', default=100, type=int) # after render_interval, the env.render() will work
+
+parser.add_argument('--max_episode', default=1000, type=int) # num of episodes for train
+
+args = parser.parse_args()
+
+
+######################################
 #environment
-EPISODES = 400
-env = gym.make('MountainCar-v0')
-env = env.unwrapped
-NUM_STATES = env.observation_space.shape[0] # 2
-NUM_ACTIONS = env.action_space.n
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+script_name = 'DQN'
+
+env = gym.make(args.env_name).unwrapped
+
+state_dim = env.observation_space.shape[0] #2
+action_dim = env.action_space.n
+
+min_Val = torch.tensor(1e-7).float().to(device) # min value
+
+directory = './Trained_Models/' + script_name + '_' + args.env_name +'/'
+
 
 #network architecture
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.fc1 = nn.Linear(NUM_STATES, 30)
+        self.fc1 = nn.Linear(state_dim, 30)
         self.fc1.weight.data.normal_(0, 0.1)
-        self.fc2 = nn.Linear(30, NUM_ACTIONS)
+        self.fc2 = nn.Linear(30, action_dim)
         self.fc2.weight.data.normal_(0, 0.1)
 
 
@@ -45,20 +76,20 @@ class Dqn():
     #init of the networks, memory space, some counters and both optimizer and loss
     def __init__(self):
         self.eval_net, self.target_net = Net(), Net()
-        self.memory = np.zeros((MEMORY_CAPACITY, NUM_STATES *2 +2))
+        self.memory = np.zeros((args.max_length_of_trajectory, state_dim *2 +2))
         # state, action ,reward and next state
         self.memory_counter = 0
         self.learn_counter = 0
-        self.optimizer = optim.Adam(self.eval_net.parameters(), LR)
+        self.optimizer = optim.Adam(self.eval_net.parameters(), args.learning_rate)
         self.loss = nn.MSELoss()
 
         self.fig, self.ax = plt.subplots()
 
     #store sequences into memory
     def store_trans(self, state, action, reward, next_state):
-        if self.memory_counter % 500 ==0:
-            print(f"The experience pool collects {self.memory_counter} time experience")
-        index = self.memory_counter % MEMORY_CAPACITY
+        #if self.memory_counter % 500 ==0:
+        #    print(f"The experience pool collects {self.memory_counter} time experience")
+        index = self.memory_counter % args.max_length_of_trajectory
         trans = np.hstack((state, [action], [reward], next_state))
         self.memory[index,] = trans
         self.memory_counter += 1
@@ -67,43 +98,36 @@ class Dqn():
     def choose_action(self, state):
         #notation that the function return the action's index nor the real action
         state = torch.unsqueeze(torch.FloatTensor(state) ,0)
-        if np.random.randn() <= EPSILON:
+        if np.random.randn() <= args.epsilon:
             action_value = self.eval_net.forward(state)
             action = torch.max(action_value, 1)[1].data.numpy() #get action whose q is max
             action = action[0] #get the action index
         else:
-            action = np.random.randint(0,NUM_ACTIONS)
+            action = np.random.randint(0,action_dim)
         return action
 
-    #plot total reward vs episode
-    def plot(self, ax, x):
-        ax.cla()
-        ax.set_xlabel("episode")
-        ax.set_ylabel("total reward")
-        ax.plot(x, 'b-')
-        plt.pause(0.000000000000001)
 
     #q update based on discounted return
     def learn(self):
         #learn 100 times then the target network update
-        if self.learn_counter % Q_NETWORK_ITERATION ==0:
+        if self.learn_counter % args.q_network_iteration ==0:
             self.target_net.load_state_dict(self.eval_net.state_dict())
         self.learn_counter+=1
         
         #sample batch of sequences
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
+        sample_index = np.random.choice(args.max_length_of_trajectory, args.batch_size)
         batch_memory = self.memory[sample_index, :]
-        batch_state = torch.FloatTensor(batch_memory[:, :NUM_STATES])
+        batch_state = torch.FloatTensor(batch_memory[:, :state_dim])
         #note that the action must be a int
-        batch_action = torch.LongTensor(batch_memory[:, NUM_STATES:NUM_STATES+1].astype(int))
-        batch_reward = torch.FloatTensor(batch_memory[:, NUM_STATES+1: NUM_STATES+2])
-        batch_next_state = torch.FloatTensor(batch_memory[:, -NUM_STATES:])
+        batch_action = torch.LongTensor(batch_memory[:, state_dim:state_dim+1].astype(int))
+        batch_reward = torch.FloatTensor(batch_memory[:, state_dim+1: state_dim+2])
+        batch_next_state = torch.FloatTensor(batch_memory[:, -state_dim:])
 
         #get q values for both state and next state but with different networks
         q_eval = self.eval_net(batch_state).gather(1, batch_action)
         q_next = self.target_net(batch_next_state).detach()
         #calculate q value for the update
-        q_target = batch_reward + GAMMA*q_next.max(1)[0].view(BATCH_SIZE, 1)
+        q_target = batch_reward + args.gamma*q_next.max(1)[0].view(args.batch_size, 1)
 
         
         loss = self.loss(q_eval, q_target)
@@ -115,29 +139,40 @@ class Dqn():
 
 def main():
     net = Dqn()
-    print("The DQN is collecting experience...")
+    print("====================================")
+    print("DQN Collection Experience...")
+    print("====================================")    
     step_counter_list = []
-    for episode in range(EPISODES):
+    for episode in range(args.max_episode):
         state = env.reset()
         step_counter = 0
+        net.memory_counter = 0
+        ep_r=0
+        episode_rewards = []
         while True:
             step_counter +=1
             env.render()
             action = net.choose_action(state)
             next_state, reward, done, info = env.step(action)
-            reward = reward * 100 if reward >0 else reward * 5
+            #reward = reward * 100 if reward >0 else reward * 5
+            ep_r += reward
+
             net.store_trans(state, action, reward, next_state)
 
-            if net.memory_counter >= MEMORY_CAPACITY:
+            if net.memory_counter >= args.max_length_of_trajectory or done:
                 net.learn()
+                print("Episode: \t{} | Episode Reward: \t{:0.2f} | Environment Step: {}".format(episode, ep_r, step_counter))
+                episode_rewards.append(ep_r)
+                
                 if done:
-                    print(f"episode {episode}, the reward is {round(reward, 3)}")
-            if done:
-                step_counter_list.append(step_counter)
-                net.plot(net.ax, step_counter_list)
+                    print("Episode: \t{} | Episode Reward: \t{:0.2f} | Environment Step: {}".format(episode, ep_r, step_counter))
+                    step_counter_list.append(step_counter)
                 break
 
             state = next_state
+    plt.plot(range(args.max_episodes),episode_reward,'k-')
+    plt.xlabel('Episode'), plt.ylabel('Episode Reward')
+        
 
 if __name__ == '__main__':
     main()
