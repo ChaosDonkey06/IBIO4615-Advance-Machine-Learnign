72c5e6b820acfb983b7730ce44dab9a43a649784
ChaosDonkey06 <je.cascante10@uniandes.edu.co>
2019-09-12 17:17:22 -0500

    fixing typo

--- 03_RNN/Tutorial.py
+++ 03_RNN/Tutorial.py
@@ -190,7 +190,7 @@ hidden_size = 256
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
 
-trainIters(encoder1, attn_decoder1,input_lang,output_lang, 75000, print_every=5000)
+trainIters(encoder1, attn_decoder1,input_lang,output_lang, n_iters=75000, print_every=1000)
 
 def showAttention(input_sentence, output_words, attentions):
     # Set up figure with colorbar
